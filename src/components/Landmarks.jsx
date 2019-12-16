368ea16bb2d813a5fec97609b9920d758f94071d
monogamus <monogamus@yahoo.com>
2019-12-16 11:02:08 +0700

    initial commit

--- src/components/Landmarks.jsx
+++ src/components/Landmarks.jsx
@@ -7,7 +7,7 @@ export default class Home extends Component {
     return (
       <div>
         <div className="content-wrapper">
-          <Image src="/assets/img/services.jpg" />
+          <Image src="/assets/img/landmarks.jpg" />
           <h2 className="heading">City landmarks</h2>
           <h5 className="sub-heading">
             With us you’ll find a perfect destination among hundreds available.

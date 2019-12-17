d3d84c0e41d000c16c84a3ad3b2c49a69fbf8ac7
bagus suryadi <50207673+monogamus@users.noreply.github.com>
2019-12-17 16:02:05 +0700

    remove city heading

--- src/components/pages/Landmarks/Landmarks.jsx
+++ src/components/pages/Landmarks/Landmarks.jsx
@@ -9,7 +9,7 @@ export default class Home extends Component {
       <div>
         <div className="content-wrapper">
           <Image src="/assets/img/landmarks.jpg" />
-          <h2 className="heading">City landmarks</h2>
+          <h2 className="heading">Landmarks</h2>
           <h5 className="sub-heading">
             With us you’ll find a perfect destination among hundreds available.
           </h5>
